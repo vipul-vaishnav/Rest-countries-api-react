cd4ae245dd32a38b678615ae050d3632a4213d6d
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-05-11 17:47:05 +0530

    Add dropdown

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Dropdown = () => {
+  return (
+    <ul
+      className="absolute w-full rounded-md shadow-lg bg-element_color z-[999] mt-2 top-full py-4
+      "
+    >
+      <li>
+        <button className="px-12 py-2 text-base font-semibold text-text_color">Africa</button>
+      </li>
+      <li>
+        <button className="px-12 py-2 text-base font-semibold text-text_color">America</button>
+      </li>
+      <li>
+        <button className="px-12 py-2 text-base font-semibold text-text_color">Asia</button>
+      </li>
+      <li>
+        <button className="px-12 py-2 text-base font-semibold text-text_color">Europe</button>
+      </li>
+      <li>
+        <button className="px-12 py-2 text-base font-semibold text-text_color">Oceania</button>
+      </li>
+    </ul>
+  );
+};
+
+export default Dropdown;
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
 
 const Filter = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleClick = () => {
+    setOpenMenu((prev) => !prev);
+  };
+
   return (
     <div>
       <div className="flex items-center justify-between gap-4 px-12 py-4 rounded-md shadow-lg text-text_color bg-element_color">
@@ -20,8 +27,11 @@ const Filter = () => {
           placeholder="Search for a country..."
         />
       </div>
-      <div className="my-12 mb-0 rounded-md shadow-lg w-fit bg-element_color">
-        <button className="flex w-full h-full px-12 py-5 text-base font-semibold gap-28 text-text_color">
+      <div className="relative my-12 mb-0 rounded-md shadow-lg w-fit bg-element_color">
+        <button
+          className="flex w-full h-full px-12 py-5 text-base font-semibold gap-28 text-text_color"
+          onClick={handleClick}
+        >
           <span>Filter by Region</span>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -34,6 +44,7 @@ const Filter = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
           </svg>
         </button>
+        {openMenu && <Dropdown />}
       </div>
     </div>
   );
